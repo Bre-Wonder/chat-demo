c1e3bb747e599d5550fc5ea434248367ab34be07
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-02 16:42:08 -0800

    added StackNavigator to toggle screens

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, NavigationContainer } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Screen1 from './Components/Screen1';
+import Screen2 from './Components/Screen2';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName='Screen1'>
+        <Stack.Screen name='Screen1' component={Screen1}/>
+        <Stack.Screen name='Screen2' component={Screen2}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
