9a6979d48aecafd69137ba8a50d6328737e362af
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-02 17:50:51 -0800

    changed navigation Container

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, NavigationContainer } from 'react-native';
+import { StyleSheet, Text, View,  } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Screen1 from './Components/Screen1';
 import Screen2 from './Components/Screen2';
