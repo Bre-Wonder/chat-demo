e72a61c9ac91a05a0624474793599399af0da4b3
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-02 17:51:17 -0800

    added component that allows navigation

---
+++ Components/Screen1.js
@@ -0,0 +1,24 @@
+import { StyleSheet, View, Text, Button } from "react-native"; 
+
+const Screen1 = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Screen 1</Text>
+      <Button
+        title="Go to Screen 2"
+        onPress={() => navigation.navigate('Screen2')}
+        />
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default Screen1;
