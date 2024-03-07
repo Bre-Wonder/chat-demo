75d3bef01f0c2ba5ba010494a6fd4a9ec24d3edc
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-06 21:26:28 -0800

    added background images + editing

--- Components/startScreen.js
+++ Components/startScreen.js
@@ -1,29 +1,28 @@
 import { useState } from "react";
-import {ImageBackground StyleSheet, View, Text, Button, TextInput } from "react-native";
-import BackgroundImage.png from './A5-chatapp-assets/BackgroundImage.png';
+import {ImageBackground, StyleSheet, View, Text, Button, TextInput } from "react-native";
+
+const image = require('../A5-chatapp-assets/Background Image.png');
 
 const StartScreen = ({ navigation }) => {
   const [name, setName] = useState('');
 
-  const image = {uri: ''};
 
   return (
     <View style={styles.container}>
-      <ImageBackground>
-      <TextInput
-        style={styles.textInput}
-        value={name}
-        onChangeText={setName}
-        placeholder='Type your username here'
-      >
-      </TextInput>
-      <Button
-        title="Go to Chat Screen"
-        onPress={() => navigation.navigate('ChatScreen', {name: name})}
-        />
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text} >Hello Start Screen</Text>
+        <TextInput
+          style={styles.textInput}
+          value={name}
+          onChangeText={setName}
+          placeholder='Type your username here'
+        >
+        </TextInput>
+        <Button
+          title="Go to Chat Screen"
+          onPress={() => navigation.navigate('ChatScreen', {name: name})}
+          />
       </ImageBackground>
-      <Text>Hello Start Screen</Text>
-      
     </View>
 
   );
@@ -32,20 +31,29 @@ const StartScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: 'center'
   },
   image: {
     flex: 1,
     justifyContent: 'center',
   },
   textInput: {
-    width: "88%",
+    width: '88%',
     padding: 15,
     borderWidth: 1,
     marginTop: 15,
-    marginBottom: 15
+    marginBottom: 15, 
+    textAlign: 'center'
+  },
+  text: {
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: '25'
   }
 });
 
 export default StartScreen;
+
+
+// justifyContent: 'center',
+// alignItems: 'center'
