4af38f2f517f135175f31a6c12b82b2d3d7a4ae4
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-15 21:09:32 -0700

    pulled in navigation of backgroundColor

--- Components/chatScreen.js
+++ Components/chatScreen.js
@@ -4,13 +4,14 @@ import { StyleSheet, View, Text } from "react-native";
 const ChatScreen = ({ route, navigation }) => {
 
   const { name } = route.params;
+  const { backgroundColor } = route.params;
 
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor}]}>
       <Text>Hello Chat Screen</Text>
     </View>
   );
