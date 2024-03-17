8cd9dd7b10bae048ca1f07985ba030a9e8c354a2
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-16 17:12:08 -0700

    trying to adjust content

--- Components/startScreen.js
+++ Components/startScreen.js
@@ -18,47 +18,49 @@ const StartScreen = ({ navigation }) => {
     <View style={styles.container}>
       {/* Adding the background image to the background of the first screen */}
       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <Text style={styles.text} > Chat Me Up</Text>
-      <View style={styles.innerContainer}>
-        {/* Username input box */}
-        <TextInput
-          style={styles.textInput}
-          value={name}
-          onChangeText={setName}
-          placeholder='Your Username Here'
-        >
-        </TextInput>
-        <Text
-          style={styles.text2}
-          placeholder='Pick a Background Color.'
-        >
-        Choose Your Background Color
-        </Text>
-        {/* List of circular buttons that determine what color the background color of the ChatScreen wiill be */}
-        <View style={styles.colorGrid}>
-          <TouchableOpacity  
-            style={[styles.circleButton, {backgroundColor: '#090C08'}]}
-            onPress={() => handColorChange ('#090C08')} >
-          </TouchableOpacity>
-          <TouchableOpacity  
-            style={[styles.circleButton, {backgroundColor: '#474056'}]}
-            onPress={() => handColorChange ('#474056')} >
-          </TouchableOpacity>
-          <TouchableOpacity  
-            style={[styles.circleButton, {backgroundColor: '#8A95A5'}]}
-            onPress={() => handColorChange ('#8A95A5')} >
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={[styles.circleButton, {backgroundColor: '#B9C6AE'}]}
-            onPress={() => handColorChange ('#B9C6AE')} >
-          </TouchableOpacity>
-        </View> 
-        {/* Button to navigage to ChatScreen */}
-        <Button
-          title="Start Chatting"
-          onPress={() => navigation.navigate('ChatScreen', {name: name, backgroundColor: backgroundColor})}
-        />
-      </View>
+      
+        <Text style={styles.text} > Chat Me Up</Text>
+        <View style={styles.innerContainer}>
+          {/* Username input box */}
+          <TextInput
+            style={styles.textInput}
+            value={name}
+            onChangeText={setName}
+            placeholder='Your Username Here'
+          >
+          </TextInput>
+          <Text
+            style={styles.text2}
+            placeholder='Pick a Background Color.'
+          >
+          Choose Your Background Color
+          </Text>
+          {/* List of circular buttons that determine what color the background color of the ChatScreen wiill be */}
+          <View style={styles.colorGrid}>
+            <TouchableOpacity  
+              style={[styles.circleButton, {backgroundColor: '#090C08'}]}
+              onPress={() => handColorChange ('#090C08')} >
+            </TouchableOpacity>
+            <TouchableOpacity  
+              style={[styles.circleButton, {backgroundColor: '#474056'}]}
+              onPress={() => handColorChange ('#474056')} >
+            </TouchableOpacity>
+            <TouchableOpacity  
+              style={[styles.circleButton, {backgroundColor: '#8A95A5'}]}
+              onPress={() => handColorChange ('#8A95A5')} >
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={[styles.circleButton, {backgroundColor: '#B9C6AE'}]}
+              onPress={() => handColorChange ('#B9C6AE')} >
+            </TouchableOpacity>
+          </View> 
+          {/* Button to navigage to ChatScreen */}
+          <Button
+            style={[styles.button, {backgroundColor: '#757083'}]}
+            title="Start Chatting"
+            onPress={() => navigation.navigate('ChatScreen', {name: name, backgroundColor: backgroundColor})}
+          />
+        </View>
       </ImageBackground>
     </View>
 
@@ -69,18 +71,27 @@ const StartScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   image: {
     flex: 1,
     justifyContent: 'center',
   },
   innerContainer: {
-    alignItems: 'center',
-    borderWidth: 1,
+    width: '88%',
+    height: '44%',
+    alignItems: 'center',  
+    justifyContent: 'space-evenly',
     backgroundColor: '#FFFFFF',
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.3
   },
   textInput: {
+    fontSize: 16,
+    fontWeight: 300,
+    color: '#757083',
+    opacity: 0.5,
     width: '88%',
     padding: 15,
     borderWidth: 1,
@@ -89,11 +100,16 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   },
   text2: {
+    fontSize: 16,
+    fontWeight: 300,
+    color: '#757083',
+    opacity: 0.5,
     width: '88%',
-    padding: 10,
+    fontSize: 17,
+    padding: 5,
     marginTop: 5,
     marginBottom: 15, 
-    textAlign: 'left'
+    textAlign: 'center'
   },
   colorGrid: {
     flexDirection: 'row',
@@ -111,6 +127,12 @@ const styles = StyleSheet.create({
     shadowColor: 'black',
     shadowOffset: {width: 0, height: 4},
     shadowOpacity: 0.3
+  },
+  button: {
+    fontSize: 16,
+    fontWeight: 600,
+    color: '#FFFFFF',
+    backgroundColor: '#757083'
   },
   text: {
     textAlign: 'center',
