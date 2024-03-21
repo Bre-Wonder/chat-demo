c73a350acc1e9f5ad169056cbb5df95f14b979aa
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-20 21:23:43 -0700

    add chat functionality from Gifted Chat

--- Components/chatScreen.js
+++ Components/chatScreen.js
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native"; 
+import { useEffect, useState } from "react";
+import { Bubble, GiftedChat, renderBubble } from "react-native-gifted-chat";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native"; 
 import StartScreen from "./StartScreen";
 
 const ChatScreen = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
 
   // Routing in username and background color from StartScreen
   const { name } = route.params;
@@ -10,11 +12,65 @@ const ChatScreen = ({ route, navigation }) => {
 
   useEffect(() => {
     navigation.setOptions({ title: name });
+
+    //using setter method - setMessages to allow useres to send back and forth messages
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello Developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+        //marks message as sent, uses one tick
+        sent: true,
+        //marked message as received, uses two ticks
+        received: true,
+        //marks message as pending, uses clock loader
+        pending: true
+      },
+      {
+        _id: 2,
+        text: 'You have enterd the chat',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
+
+  // changes the color of the message bubbles from user to user
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
+
+  //takes the setter method and uses it to present previous message along with new messages sent
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
 
   return (
     <View style={[styles.container, {backgroundColor}]}>
-      <Text>Hello Chat Screen</Text>
+      <GiftedChat  
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}      
+      />
+      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
     </View>
   );
 }
@@ -23,8 +79,6 @@ const ChatScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   }
 });
 
