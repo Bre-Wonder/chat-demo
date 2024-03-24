fd16802c6af26e261a62808e4dd07b1d136675f6
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-23 20:52:42 -0700

    added Firestore to app

--- App.js
+++ App.js
@@ -2,12 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,  } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
 import StartScreen from './Components/StartScreen';
 import ChatScreen from './Components/ChatScreen';
 
+//Create Navigator move between screens
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBfQtN1uImKV78l4LQT1143iafwdPZr8x0",
+    authDomain: "chat-app-825d1.firebaseapp.com",
+    projectId: "chat-app-825d1",
+    storageBucket: "chat-app-825d1.appspot.com",
+    messagingSenderId: "546065501245",
+    appId: "1:546065501245:web:8e2fe566899d1340612e54"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  //Initialize Cloud FireStore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
 
     // Added NavigationContainer in order for user to change screens
